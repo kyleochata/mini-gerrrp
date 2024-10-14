45e3bdf15fe4f9f2eff0db2960bf4135e7c6ca8f
kyleochata <kyleochata@gmail.com>
2024-10-14 12:00:24 -0700

    unary responses working

---
+++ client/unary.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/kyleochata/mini-gerp/proto"
+)
+
+func callSayHello(client pb.GreetServiceClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	res, err := client.SayHello(ctx, &pb.NoParam{})
+	if err != nil {
+		log.Fatalf("Failed to call SayHello: %v", err)
+	}
+	log.Printf("Response from server: %s", res.Message)
+}
